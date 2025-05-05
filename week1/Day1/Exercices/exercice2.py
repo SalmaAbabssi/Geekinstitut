9a28f1d60d8e264ded4a00b50463ee85c6bbfe32
SalmaAbabssi <salmaababssi5@gmail.com>
2025-05-05 17:58:02 +0100

    add day1

---
+++ week1/Day1/Exercices/exercice2.py
@@ -0,0 +1,4 @@
+a= 99
+b= 3
+result = (a**b)*8
+print(result)
---
+++ week1/Day1/Exercices/exercice3.py
@@ -0,0 +1,6 @@
+b ="salma"
+a= input("Enter your name: ")
+if a == b:
+    print ("it's the same name")
+else:
+    print("Nice to meet you, {b}! That's a cool name, even if it's not mine 😄")
