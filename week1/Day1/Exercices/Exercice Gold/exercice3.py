6b39d1c64a6454ed882c2cbc577c064deec7cd47
SalmaAbabssi <salmaababssi5@gmail.com>
2025-05-07 23:58:10 +0100

    Day1

---
+++ week1/Day1/Exercices/Exercice Gold/exercice3.py
@@ -0,0 +1,8 @@
+my_name = "salma"
+
+user_name = input('enter ur name : ')
+
+while user_name != my_name:
+    print("we dont have the same name")
+    user_name = input('enter ur name : ')
+    break
---
+++ week1/Day1/Exercices/Exercice Gold/exercice6.py
@@ -0,0 +1,20 @@
+import random
+
+#! Ask the user to input a number from 1 to 9 (including).
+number = int(input("enter a number from 1 to 9: "))
+
+#! Get a random number between 1 and 9. Hint: random module.
+random_number = random.randint(1, 9)  
+# randint : means including 1 and 9 and whant between them
+
+# print(random_number)
+
+#! If the user guesses the correct number print a message 
+#! that says “Winner”.
+#! If the user guesses the wrong number print a message 
+#! that says “Better luck next time.”
+
+if number == random_number:
+    print('winner')
+else: 
+    print('better luck next time')
