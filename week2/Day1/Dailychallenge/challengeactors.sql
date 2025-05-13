7e13832297a933ba5398deb7e3f9566403d20421
SalmaAbabssi <salmaababssi5@gmail.com>
2025-05-13 10:56:47 +0100

    challengeactors

---
+++ week2/Day1/Dailychallenge/challengeactors.sql
@@ -0,0 +1,27 @@
+-- Database: Hollywood
+
+-- DROP DATABASE IF EXISTS "Hollywood";
+
+CREATE DATABASE "Hollywood"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'French_France.1252'
+    LC_CTYPE = 'French_France.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
+	CREATE TABLE actors(
+ actor_id SERIAL PRIMARY KEY,
+ first_name VARCHAR (50) NOT NULL,
+ last_name VARCHAR (100) NOT NULL,
+ age DATE NOT NULL,
+ number_oscars SMALLINT NOT NULL
+)
+select count (*) from actors 
+
+INSERT INTO actors (first_name, last_name, age, number_oscars)
+VALUES('','','', );
+--
